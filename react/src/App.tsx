5cfdbe2585e22ace96cf7dbeaca67f05ee32160e
Alex Balak <alex.balak@outlook.com>
2025-12-13 11:53:25 +0100

    added link to create-post n front

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -48,6 +48,12 @@ export default function App() {
                   My Posts
                 </Link>
                 <Link
+                  to="/create-post"
+                  className="text-gray-500 hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Create Post
+                </Link>
+                <Link
                   to="/login"
                   className="text-gray-500 hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium"
                 >
